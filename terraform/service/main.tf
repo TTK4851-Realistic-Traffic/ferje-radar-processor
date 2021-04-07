16916e06e9b68d4d69d9b97049195ea8834a6ff6
Fredrik F. Lindhagen <fredrik@lindhagen.io>
2021-04-07 14:13:22 +0200

    Removed unused service folder

--- terraform/service/main.tf
+++
@@ -1,8 +0,0 @@
-////
-// Service
-//
-// Setup any additional resources, necessary for development
-// but not related to any specific deployment/environment.
-//
-// This include setting up an Elastic Container Registry
-////
